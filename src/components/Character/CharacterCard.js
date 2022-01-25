67f2e058ad65cf39b98ba813561f5bf35cf05ac7
GitTher <estherfernandezflorez@gmail.com>
2022-01-25 17:59:06 +0100

    add character card for each character

--- src/components/Character/CharacterCard.js
+++ src/components/Character/CharacterCard.js
@@ -1,6 +1,11 @@
-const CharacterCard = () => {
+const CharacterCard = (props) => {
     return (
-        "CharacterCard"
+        <article>
+            <img src={props.character.image !== "" ? props.character.image : `https://via.placeholder.com/210x295/ffffff/666666/?text=${props.character.name}`} title={props.character.name} alt={props.character.name} />
+            <h3>{props.character.name}</h3>
+            <p>{props.character.species}</p>
+
+        </article>
     )
 }
 
