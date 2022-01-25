c2391bcab489bead3b534c23bfd4f3c395a5820f
GitTher <estherfernandezflorez@gmail.com>
2022-01-25 16:40:20 +0100

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+// import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+// import '../styles/App.scss';
+// import PropTypes from 'prop-types';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
