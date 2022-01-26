3f53b8c11952a960f9a006087e31fd9db494dc33
GitTher <estherfernandezflorez@gmail.com>
2022-01-26 15:36:32 +0100

    update github pages

--- src/services/callToApi.js
+++ src/services/callToApi.js
@@ -1,5 +1,5 @@
 const callToApi = (house) => {
-    return fetch(`http://hp-api.herokuapp.com/api/characters/house/${house}`)
+    return fetch(`https://hp-api.herokuapp.com/api/characters/house/${house}`)
         .then((response) => response.json())
         .then((data) => {
             const charactersData = data.map((character, index) => {
