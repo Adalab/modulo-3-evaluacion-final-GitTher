404c56471251edfc99c1be6f314c2e438ab3c77f
GitTher <estherfernandezflorez@gmail.com>
2022-01-26 13:24:18 +0100

    add return link to not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
     return (
-        <p> La página que buscas no existe</p>
+        <>
+            <Link to="/">Volver</Link>
+            <p> La página que buscas no existe</p>
+        </>
     )
 }
 
